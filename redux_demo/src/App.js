95e759748343be1119115f0da5d2d09e62a7e091
Niji-2001 <nijimjoy2001@gmail.com>
2024-09-21 13:14:44 +0530

    App changes

--- redux_demo/src/App.js
+++ redux_demo/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import FirstExample from "./components/CakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <FirstExample />
+        <HooksCakeContainer/>
       </div>
     </Provider>
   );
